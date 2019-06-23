d7d12169122d8d4386e51765c017d4a146fe8155
James Messinger <james.messinger@gmail.com>
2019-06-23 08:53:42 -0500

    Added rehype-slug for testing purposes

--- test/utils/process.js
+++ test/utils/process.js
@@ -3,6 +3,7 @@
 const toc = require("../../");
 const unified = require("unified");
 const parse = require("rehype-parse");
+const slug = require("rehype-slug");
 const stringify = require("rehype-stringify");
 
 module.exports = process;
@@ -10,11 +11,15 @@ module.exports = process;
 /**
  * Processes the given HTML using Rehype and the TOC plugin
  */
-async function process (html, options) {
-  let processor = unified()
-    .use(parse)
-    .use(toc, options)
-    .use(stringify);
+async function process (html, { slug: useSlug, ...options } = {}) {
+  let processor = unified().use(parse);
+
+  if (useSlug) {
+    processor.use(slug);
+  }
+
+  processor.use(toc, options);
+  processor.use(stringify);
 
   let file = await processor.process(html);
   return file.contents;
