e201818b4bf1184c564aa41d2226373d5c679677
James Messinger <james.messinger@gmail.com>
2019-06-23 10:45:08 -0500

    Added tests for the `customizeTOC` option

---
+++ test/specs/customize.spec.js
@@ -0,0 +1,152 @@
+"use strict";
+
+const process = require("../utils/process");
+const compare = require("../utils/compare");
+
+describe("options.customizeTOC", () => {
+
+  it("should modify the TOC in-place", async () => {
+    let results = await process(`
+      <html>
+        <body>
+          <h1>One</h1>
+          <h2>Two</h2>
+          <h3>Three</h3>
+          <h4>Four</h4>
+        </body>
+      </html>
+    `,
+    {
+      customizeTOC (toc) {
+        toc.tagName = "ul";
+        toc.properties.class = "my-custom-class";
+        toc.properties.id = "my-custom-id";
+        toc.children[0].children.splice(1, 1);
+      },
+    });
+
+    compare(results, `
+      <html>
+        <head></head>
+        <body>
+          <ul class="my-custom-class" id="my-custom-id">
+            <li class="toc-item toc-item-h1">
+              <a class="toc-link toc-link-h1" href="#">One</a>
+            </li>
+          </ul>
+
+          <h1>One</h1>
+          <h2>Two</h2>
+          <h3>Three</h3>
+          <h4>Four</h4>
+        </body>
+      </html>
+    `);
+  });
+
+  it("should replace the TOC with a new node", async () => {
+    let results = await process(`
+      <html>
+        <body>
+          <h1>One</h1>
+          <h2>Two</h2>
+          <h3>Three</h3>
+          <h4>Four</h4>
+        </body>
+      </html>
+    `,
+    {
+      customizeTOC (toc) {
+        return {
+          type: "element",
+          tagName: "div",
+          properties: {
+            id: "table-of-contents",
+          },
+          children: [
+            toc.children[0].children[0],
+            toc.children[0].children[1].children[0].children[0],
+          ]
+        };
+      },
+    });
+
+    compare(results, `
+      <html>
+        <head></head>
+        <body>
+          <div id="table-of-contents">
+            <a class="toc-link toc-link-h1" href="#">One</a>
+            <a class="toc-link toc-link-h2" href="#">Two</a>
+          </div>
+
+          <h1>One</h1>
+          <h2>Two</h2>
+          <h3>Three</h3>
+          <h4>Four</h4>
+        </body>
+      </html>
+    `);
+  });
+
+  it("should remove the TOC entirely by returning false", async () => {
+    let results = await process(`
+      <html>
+        <body>
+          <h1>One</h1>
+          <h2>Two</h2>
+          <h3>Three</h3>
+          <h4>Four</h4>
+        </body>
+      </html>
+    `,
+    {
+      customizeTOC () {
+        return false;
+      },
+    });
+
+    compare(results, `
+      <html>
+        <head></head>
+        <body>
+          <h1>One</h1>
+          <h2>Two</h2>
+          <h3>Three</h3>
+          <h4>Four</h4>
+        </body>
+      </html>
+    `);
+  });
+
+  it("should remove the TOC entirely by returning null", async () => {
+    let results = await process(`
+      <html>
+        <body>
+          <h1>One</h1>
+          <h2>Two</h2>
+          <h3>Three</h3>
+          <h4>Four</h4>
+        </body>
+      </html>
+    `,
+    {
+      customizeTOC () {
+        return null;
+      },
+    });
+
+    compare(results, `
+      <html>
+        <head></head>
+        <body>
+          <h1>One</h1>
+          <h2>Two</h2>
+          <h3>Three</h3>
+          <h4>Four</h4>
+        </body>
+      </html>
+    `);
+  });
+
+});
